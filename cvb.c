46a59c469bbfedd035e6c5d2bcdc295b64bd505b
Your Name <you@example.com>
2024-11-25 09:55:20 +0530

    while loop

--- cvb.c
+++ cvb.c
@@ -1,17 +1,18 @@
 #include<stdio.h>
 int main()
 {
- int num;
- printf("enter a num");
- scanf("%d",&num);
- if (num>0)
+ int x;
+ printf("enter a x");
+ scanf("%d",&x);
+ if (x>0)
  {
- printf("The num is positive\n");
+ printf("The x is positive\n");
  }
  else 
  {
  printf("error\n");
  }
+ /*
  int x=num%10;
  {
  printf("unit's place num%d\n",x);
@@ -29,6 +30,15 @@ int main()
  int a=x+y+z;
  {
  printf("addition of num's is %d\n", a);
+ }
+ */
+ 
+ while (x>0)
+ {
+ int temp=x%10;
+ int k=x/10;
+ x=k;
+ {printf("value of temp %d\n",temp);}
  }
  return 0;
  }
