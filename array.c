b8923b28e396e83acc81b3a7bd534d13e18a5946
Your Name <you@example.com>
2024-12-04 09:38:53 +0530

    binary search

--- array.c
+++ array.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 int main()
 {
- int i, search,flag=0;
+ int i, search,flag=0,low,high,mid,s;
  printf("enter a number to search in array:\n");
  scanf("%d",&search);
- int b[7]={1,2,3,4,5,6,7};
-  for (int i=0; i<7; i++)
+ int b[12]={1,2,3,4,5,6,7,8,9,10,11,12};
+  for (int i=0; i<10; i++)
  {
     if(b[i]==search)
     {
@@ -26,6 +26,24 @@ if(flag==-1)
  {
  	printf("given number is not present in the array\n");
  }
-
+ 
+ 
+ low= 0, high = 11;
+ 
+ mid = (low+high)/2;
+ 
+ if(search>b[mid])
+ {
+ 	printf("number is at right side\n");
+ }
+ else if(search<b[mid])
+ {
+ 	printf("number is at left side\n");
+ }
+ else
+ {
+ 	printf("number is located :%d\n", mid);
+ }
+ 
     return 0;
  }
